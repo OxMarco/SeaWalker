0282694eaeb45beff9ddaa0eb0d4e7545ce8eb56
grcasanova <giammarco.casanova@gmail.com>
2018-09-05 10:42:00 +0200

    fixes

---
+++ NavigationSystem/WorldState/WeatherMgr/SunTracker.cpp
@@ -0,0 +1,9 @@
+//
+//  SunTracker.cpp
+//  SeaWalker
+//
+//  Created by Giammarco René Casanova on 05/09/2018.
+//  Copyright © 2018 seawalker. All rights reserved.
+//
+
+#include "SunTracker.hpp"
---
+++ NavigationSystem/WorldState/WeatherMgr/SunTracker.hpp
@@ -0,0 +1,14 @@
+//
+//  SunTracker.hpp
+//  SeaWalker
+//
+//  Created by Giammarco René Casanova on 05/09/2018.
+//  Copyright © 2018 seawalker. All rights reserved.
+//
+
+#ifndef SunTracker_hpp
+#define SunTracker_hpp
+
+#include <stdio.h>
+
+#endif /* SunTracker_hpp */
